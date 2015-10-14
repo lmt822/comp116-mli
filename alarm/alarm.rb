870c57fafa1908c5e99261cfee4c602640fe09fc
Mengtian Li <mengtianli@wr-130-64-179-90.medford.tufts.edu>
2015-10-14 01:18:44 -0400

    update alarm

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,6 +1,6 @@
 require 'packetfu'
 require 'apachelogregex'
-
+require 'base64'
 def read_logfile(filename)
 	inicident_num = 1
 	# from github.com/weppos/apachelogregex
@@ -46,7 +46,59 @@ def read_logfile(filename)
 end
 
 def check_live_stream()
-
+	inicident_num = 1
+	stream = PacketFu::Capture.new(:start => true, :iface => 'eth0', :promisc => true)
+	stream.stream.each do |p|
+		curr_pkt = PacketFu::Packet.parse(p)
+		if !curr_pkt.is_ip? or (curr_pkt.class.name != "PacketFu::TCPPacket" and curr_pkt.class.name != "PacketFu::UDPPacket" and curr_pkt.class.name != "PacketFu:ARPPacket")
+			next
+		end
+		tcp_flag = nil
+		pkt_body = curr_pkt.payload
+		puts pkt_body
+		if (curr_pkt.class.name == "PacketFu:TCPPacket")
+			tcp_flag = curr_pkt.tcp_header.tcp_flags.to_i
+			if tcp_flag == 0
+				#Null scan
+				payload = pkt_body
+				puts "#{inicident_num}. ALERT: NULL scan is detected from #{curr_pkt.ip_header.ip_saddr} (#{curr_pkt.proto.last} (#{payload})!"
+				inicident_num += 1
+			elsif tcp_flag == 1
+				#FIN scan
+				payload = pkt_body
+				puts "#{inicident_num}. ALERT: FIN scan is detected from #{curr_pkt.ip_saddr} (#{curr_pkt.proto.last} (#{payload})!"
+				inicident_num += 1
+			elsif tcp_flag == 41
+				#XSS scan
+				payload = pkt_body
+				puts "#{inicident_num}. ALERT: Xmas scan is detected from #{curr_pkt.ip_saddr} (#{curr_pkt.proto.last} (#{payload})!"
+				inicident_num += 1
+			end
+		end
+		if pkt_body != ""
+			#credit card regex
+			credit_card_regex = ['4\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}','5\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}','6011(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}','3\d{3}(\s|-)?\d{6}(\s|-)?\d{5}','\d{4}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}']
+			if /Nmap/.match(pkt_body)
+				#Other Nmap scan
+				payload = base64.encode64(pkt_body)
+				puts "#{inicident_num}. ALERT: Nmap scan is detected from #{curr_pkt.ip_saddr} (#{curr_pkt.proto.last}! (#{payload})"
+				inicident_num += 1
+			elsif /Nikto/.match(pkt_body)
+				#Nikto scan
+				payload = base64.encode64(pkt_body)
+				puts "#{inicident_num}. ALERT: Nikto scan is detected from #{curr_pkt.ip_saddr} (#{curr_pkt.proto.last}! (#{payload})"
+				inicident_num += 1
+			end
+			credit_card_regex.each do |reg|
+				if "/#{reg}/".match(pkt_body) 
+					# credit card leak
+					payload = base64.encode64(pkt_body)
+					puts "#{inicident_num}. ALERT: Credit card leak is detected from #{curr_pkt.ip_saddr} (#{curr_pkt.proto.last}! (#{payload})"
+					inicident_num += 1
+				end
+			end
+		end
+	end
 end
 
 
@@ -55,9 +107,8 @@ if ARGV[0] == '-r' && ARGV[1] != nil
 	filename = ARGV[1]
 	read_logfile(filename)
 elsif ARGV[0] == nil
-#	check_live_stream()
+	check_live_stream()
 else
 	puts 'wrong'
 end
-#stream = PacketFu::Capture.new(:start => true, :iface => 'eth0', :promisc => true)
-#stream.show_live()
+
