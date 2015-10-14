99f32ffebb49956947eccf663a866b37a7b117e3
Mengtian Li <mengtianli@wr-130-64-179-90.medford.tufts.edu>
2015-10-13 23:39:08 -0400

    Add alarm

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,15 +1,48 @@
 require 'packetfu'
-$inicident_num = 0
+require 'apachelogregex'
 
 def read_logfile(filename)
-	f = File.open(filename, 'r')
-	f.each_line do |line|
-		# parse log file
-		pattern = /^(\S+).*\s(.*)\s\S*\s(\d+)/
-		string = pattern.match(line)
-		puts string[0]
-	end
+	inicident_num = 1
+	# from github.com/weppos/apachelogregex
+ 	format = '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
+ 	parser = ApacheLogRegex.new(format)
 
+ 	File.open(filename, 'r') do |f|
+		f.each_line do |line|
+			# parse log file
+			log = parser.parse(line)
+			# get ip and payload
+			ip = log["%h"]
+			payload = "#{log["%r"]} #{log["%{Referer"]} #{log["%{User-Agent}i"]}"
+			# for protocol extraction, asked Carrie Xu
+			protocol = line.split(' ')[-1].split('/')[0]
+			if payload ==''
+				protocol = 'HTTP'
+			elsif protocol != 'HTTP' and protocol != 'RSTP' and protocol != 'HTTPS'
+				protocol = 'HTTP'
+			end
+
+			if /Nmap/.match(log["%{User-Agent}i"])
+				puts "#{inicident_num}. ALERT: Nmap scan is detected from #{ip} (#{protocol}) (#{payload}!"
+				inicident_num += 1		
+			elsif /Nikto/.match(log["%{User-Agent}i"])
+				puts "#{inicident_num}. ALERT: Nikto is detected from #{ip} (#{protocol}) (#{payload}!"
+				inicident_num += 1
+			elsif /masscan/.match(log["%{User-Agent}i"])
+				puts "#{inicident_num}. ALERT: Rob Graham's Masscan is detected from #{ip} (#{protocol}) (#{payload}!"
+				inicident_num += 1
+			elsif /shellshock/.match(log["%{User-Agent}i"])
+				puts "#{inicident_num}. ALERT: Shellshock is detected from #{ip} (#{protocol}) (#{payload}!"
+				inicident_num += 1
+			elsif /phpMyAdmin/.match(log['%r'])
+				puts "#{inicident_num}. ALERT: pertaining phpMyAdmin is detected from #{ip} (#{protocol}) (#{payload}!"
+				inicident_num += 1
+			elsif /\\x[0-9a-fA-f]{2}/.match(log['%r'])
+				puts "#{inicident_num}. ALERT: Shellcode is detected from #{ip} (#{protocol}) (#{payload}!"
+				inicident_num += 1
+			end
+		end
+	end
 end
 
 def check_live_stream()
@@ -20,13 +53,11 @@ end
 
 if ARGV[0] == '-r' && ARGV[1] != nil
 	filename = ARGV[1]
-	puts '1'
 	read_logfile(filename)
 elsif ARGV[0] == nil
-	puts '2'
 #	check_live_stream()
 else
-	puts '3'
+	puts 'wrong'
 end
 #stream = PacketFu::Capture.new(:start => true, :iface => 'eth0', :promisc => true)
 #stream.show_live()
